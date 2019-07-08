2c9a35b0ba7456fff8e0dddeda294f118eaa8546
paulo.almeida-at-424057606549 <paulo.almeida@erpflex.com.br>
2019-07-08 15:38:53 -0300

    new class

--- src/ReadLayoutCnab.php
+++ src/ReadLayoutCnab.php
@@ -3,8 +3,40 @@ namespace LayoutCnabRetorno;
 
 
 class ReadLayoutCnab {
-    public function __construct()
+
+    public $is_valid;
+    private $file;
+    private $layout;
+
+    public function __construct($file, $layout)
     {
+        $this->file = $file;
+        $this->layout = $layout;
+        
     }
+
+    public function leituraHeader()
+    {
+      $layout = $this->layout;
+      foreach($layout as $item) {
+          if($item['tipo'] == 'header'){
+            $header["{$item['nomeCampo']}"] = $this->leituraLinha($this->file,$item['posicao_inicial'],$item['tamanho_campo']);
+          }
+      }
+    }
+    private function leituraLinha($line, $start, $size)
+    {
+        return substr($line,$start,$size);
+    }
+
+    protected function validateFile()
+    {
+        $file = $this->file;
+        if(!$file) {
+            throw new \Exception("Não foi encontrado nenhum arquivo para validar");
+        }
+        
+    }
+
     
 }
