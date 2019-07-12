2a77a08995add3139a46abac9f3f9846e06ba1a6
paulo.almeida-at-424057606549 <paulo.almeida@erpflex.com.br>
2019-07-12 12:05:17 -0300

    new rotines

--- src/LayoutCnabRetorno/ReadLayoutCnab.php
+++ src/LayoutCnabRetorno/ReadLayoutCnab.php
@@ -1,4 +1,12 @@
 <?php 
+/*
+ * This file is part of the layout-cnab-retorno package.
+ *
+ * (c) Paulo Almeida <paulo@actio.net.br>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 namespace LayoutCnabRetorno;
 
 
